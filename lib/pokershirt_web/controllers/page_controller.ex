0064c0b50532f3931557636a0efe5790628a4ebe
Andrew Fecheyr <andrew@bedesign.be>
2019-05-01 00:20:42 +0200

    Simple button to get to new room

--- lib/pokershirt_web/controllers/page_controller.ex
+++ lib/pokershirt_web/controllers/page_controller.ex
@@ -2,6 +2,14 @@ defmodule PokershirtWeb.PageController do
   use PokershirtWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", new_room_id: rand_chars(5))
+  end
+
+  defp rand_chars(length) do
+    for _ <- 0..length do
+      Integer.to_string(:rand.uniform(26) + 10, 36)
+      |> String.downcase
+    end
+    |> Enum.join
   end
 end
