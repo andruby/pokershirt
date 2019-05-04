44b251c63c364c1e0327cf8c8f8fe6eb7603e62f
Andrew Fecheyr <andrew@bedesign.be>
2019-05-05 00:14:00 +0200

    WIP session uuid

--- lib/pokershirt_web/controllers/room_controller.ex
+++ lib/pokershirt_web/controllers/room_controller.ex
@@ -3,7 +3,18 @@ defmodule PokershirtWeb.RoomController do
   alias Phoenix.LiveView
   alias PokershirtWeb.RoomLive.Show
 
+  # TODO: Move this to "ApplicationController"?
+  plug :ensure_session_uuid
+
   def show(conn, %{"id" => room_id}) do
     LiveView.Controller.live_render(conn, Show, session: %{room_id: room_id})
+  end
+
+  defp ensure_session_uuid(conn, _) do
+    case get_session(conn, :uuid) do
+      # TODO: Must be secure uuid
+      nil -> put_session(conn, :uuid, "lool")
+      uuid -> conn
+    end
   end
 end
