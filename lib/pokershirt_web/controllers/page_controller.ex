55119029e2753e0e358c9b8d85ef8fa922cd3832
Andrew Fecheyr <andrew@bedesign.be>
2019-05-08 15:25:03 +0200

    Use words for random room names

--- lib/pokershirt_web/controllers/page_controller.ex
+++ lib/pokershirt_web/controllers/page_controller.ex
@@ -1,15 +1,8 @@
 defmodule PokershirtWeb.PageController do
   use PokershirtWeb, :controller
+  alias Pokershirt.Haiku
 
   def index(conn, _params) do
-    render(conn, "index.html", new_room_id: rand_chars(5))
-  end
-
-  defp rand_chars(length) do
-    for _ <- 0..length do
-      Integer.to_string(:rand.uniform(26) + 10, 36)
-      |> String.downcase
-    end
-    |> Enum.join
+    render(conn, "index.html", new_room_id: Haiku.room_name)
   end
 end
