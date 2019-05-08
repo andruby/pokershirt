4721172378277c883d79e31b9b8318fd5afc6e2a
Andrew Fecheyr <andrew@bedesign.be>
2019-05-08 14:03:21 +0200

    Fix test db config

--- config/test.exs
+++ config/test.exs
@@ -2,8 +2,7 @@ use Mix.Config
 
 # Configure your database
 config :pokershirt, Pokershirt.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env["USER"],
   database: "pokershirt_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
