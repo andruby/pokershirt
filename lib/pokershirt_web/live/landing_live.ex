31249e3dc5ed693bac836ff8188141dafee08b2f
Andrew Fecheyr <andrew@bedesign.be>
2019-05-08 19:11:08 +0200

    Fetch landing page metrics asynchronously because Gigalixir db is slow

--- lib/pokershirt_web/live/landing_live.ex
+++ lib/pokershirt_web/live/landing_live.ex
@@ -10,12 +10,21 @@ defmodule PokershirtWeb.LandingLive do
   def mount(session, socket0) do
     socket = socket0
     |> assign(:new_room_id, session.new_room_id)
-    |> assign(:total_rounds, Pokershirt.Metric.get(Repo, :total_rounds))
-    |> assign(:total_votes, Pokershirt.Metric.get(Repo, :total_votes))
+    |> assign(:total_rounds, 0)
+    |> assign(:total_votes, 0)
 
+    async_fetch_metrics()
     Phoenix.PubSub.subscribe(Pokershirt.PubSub, "metric:total_rounds")
     Phoenix.PubSub.subscribe(Pokershirt.PubSub, "metric:total_votes")
     {:ok, socket}
+  end
+
+  defp async_fetch_metrics() do
+    pid = self()
+    Task.start(fn ->
+      send pid, {"metric_updated", :total_rounds, Pokershirt.Metric.get(Repo, :total_rounds)}
+      send pid, {"metric_updated", :total_votes, Pokershirt.Metric.get(Repo, :total_votes)}
+    end)
   end
 
   def handle_info({"metric_updated", key, value}, socket) do
