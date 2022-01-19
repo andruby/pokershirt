efcd8ecb555ef373ea25de693a2e326381b384a5
Andrew Fecheyr <andrew@bedesign.be>
2022-01-19 12:13:53 +0100

    Test config

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :pokershirt, Pokershirt.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "pokershirt",
+  password: "pokershirt",
   database: "pokershirt_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox,
