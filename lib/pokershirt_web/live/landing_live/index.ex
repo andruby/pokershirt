e81cf7340f104533cc39b2265663abcaac1437ea
Andrew Fecheyr <andrew@bedesign.be>
2022-01-21 21:34:50 +0100

    Remove flickr from homepage by fetching data synchronously

--- lib/pokershirt_web/live/landing_live/index.ex
+++ lib/pokershirt_web/live/landing_live/index.ex
@@ -1,14 +1,14 @@
 defmodule PokershirtWeb.LandingLive.Index do
   use PokershirtWeb, :live_view
   alias Pokershirt.Repo
+  alias Pokershirt.Metric
 
   def mount(_params, _session, socket) do
     socket2 = socket
     |> assign(:new_room_id, "testroom")
-    |> assign(:total_rounds, 0)
-    |> assign(:total_votes, 0)
+    |> assign(:total_rounds, Metric.get(Repo, :total_rounds))
+    |> assign(:total_votes, Metric.get(Repo, :total_votes))
 
-    async_fetch_metrics()
     Phoenix.PubSub.subscribe(Pokershirt.PubSub, "metric:total_rounds")
     Phoenix.PubSub.subscribe(Pokershirt.PubSub, "metric:total_votes")
     {:ok, socket2}
@@ -16,13 +16,5 @@ defmodule PokershirtWeb.LandingLive.Index do
 
   def handle_info({"metric_updated", key, value}, socket) do
     {:noreply, assign(socket, key, value)}
-  end
-
-  defp async_fetch_metrics() do
-    pid = self()
-    Task.start(fn ->
-      send pid, {"metric_updated", :total_rounds, Pokershirt.Metric.get(Repo, :total_rounds)}
-      send pid, {"metric_updated", :total_votes, Pokershirt.Metric.get(Repo, :total_votes)}
-    end)
   end
 end
